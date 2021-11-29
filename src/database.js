6a9ac39dd4a1b56821031f13e5886c394ca214bd
Mario <cl.marioandres@gmail.com>
2021-11-29 14:48:31 -0300

    Cambios en parametros de conexion a bases de datos por variables de entorno

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
 const mongo = require("mongoose");
 
-
-const { MONGODB_HOST, MONGODB_DATABASE, MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+const { MONGODB_HOST, MONGODB_DATABASE, MONGODB_USERNAME, MONGODB_PASSWORD } =
+  process.env;
 const MONGODB_URL = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`;
 
-mongo.connect(MONGODB_URL, {
-   useUnifiedTopology: true,
-   useNewUrlParser: true,
-   authSource: "admin",
-   useCreateIndex: true
-
-})
-   .then(db => console.log('MongoDB connected...'))
-   .catch(err => console.log(err));
+mongo
+  .connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    authSource: "admin",
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
--- src/sequelize.js
+++ src/sequelize.js
@@ -1,18 +1,14 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from "sequelize";
+const { PGSQL_SERVER } = process.env;
 
-export const sequelize = new Sequelize(
-   'postgres',
-   'postgres',
-   'h0landa3028',
-   {
-      host: '192.168.1.162',
-      dialect: 'postgres',
-      pool:{
-         max: 5,
-         min: 0,
-         require: 30000,
-         idle: 10000
-      },
-      logging: false
-   }
-);
+export const sequelize = new Sequelize("postgres", "postgres", "h0landa3028", {
+  host: PGSQL_SERVER,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    require: 30000,
+    idle: 10000,
+  },
+  logging: false,
+});
