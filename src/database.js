d4205cdf0768acab6f44707b27e693ab37b654fc
Mario A. Brito P <cl.marioandres@gmail.com>
2022-02-01 12:57:55 -0300

    pruebas de configuracion de deployment en heroku con mongodb atlas

--- src/database.js
+++ src/database.js
@@ -6,7 +6,7 @@ const {
 	MONGODB_USERNAME,
 	MONGODB_PASSWORD,
 } = process.env;
-export const MONGODB_URL = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`;
+export const MONGODB_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`;
 
 mongo
 	.connect(MONGODB_URL, {
