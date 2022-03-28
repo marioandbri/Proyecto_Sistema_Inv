047006bf17b95fdb11255f00d6e5a83b7d1c1991
Mario A. Brito P <cl.marioandres@gmail.com>
2022-03-28 17:53:44 -0300

    depuración de codigo comentado para conexion de sequelize en index.js

--- src/index.js
+++ src/index.js
@@ -4,29 +4,8 @@ const app = require("./server").default;
 require("./database");
 require("./session");
 
-// const { sequelize } = require("./sequelize.js");
-//Server is listening
 app.listen(app.get("port"), () => {
 	console.log("Server en puerto", app.get("port"));
-	// sequelize
-	//   .authenticate()
-	//   .then(() => {
-	//     console.log("Postgres connected...");
-	//   })
-	//   .catch((e) => {
-	//     console.log("No se ha podido conectar a postgres", e);
-	//   });
-	//   if(process.env.NODE_ENV === "development"){
-
-	//     sequelize
-	//       .sync({ alter: true })
-	//       .then(() => {
-	//         console.log("Alterations executed correctly");
-	//       })
-	//       .catch((e) => {
-	//         console.log("Something unexpected happen when doing alterations", e);
-	//       });
-	//   }
 });
 
 console.log(process.env.NODE_ENV);
